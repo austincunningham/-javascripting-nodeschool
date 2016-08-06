8ec3802d1ed528fe8dfdd658238182076cd6de62
austincunningham <austincunningham@hotmail.com>
2016-08-06 23:35:36 +0100

    http upper caser

---
+++ learnyounode/http-uppercaser.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var map = require('through2-map')
+
+var port = process.argv[2];
+var path = process.argv[3];
+     var server = http.createServer(function (req, res) {
+       // request handling logic...
+       req.pipe(map(function (chunk){
+       	return chunk.toString().toUpperCase();
+       })).pipe(res);
+     })
+     server.listen(port);
+
+/** Here's the official solution in case you want to compare notes:
+
+ ─────────────────────────────────────────────────────────────────────────────
+
+     var http = require('http')
+     var map = require('through2-map')
+
+     var server = http.createServer(function (req, res) {
+       if (req.method != 'POST')
+         return res.end('send me a POST\n')
+
+       req.pipe(map(function (chunk) {
+         return chunk.toString().toUpperCase()
+       })).pipe(res)
+     })
+
+     server.listen(Number(process.argv[2]))
+
+ ─────────────────────────────────────────────────────────────────────────────
+ */
