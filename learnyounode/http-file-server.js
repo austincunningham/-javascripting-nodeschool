67412f79fd5bc67641e26edf314c7027048c5e69
austincunningham <austincunningham@hotmail.com>
2016-08-06 23:15:15 +0100

    http file server added varables for code readablity

--- learnyounode/http-file-server.js
+++ learnyounode/http-file-server.js
@@ -1,11 +1,13 @@
 var http = require('http');
 var fs = require('fs');
+var port = process.argv[2];
+var path = process.argv[3];
      var server = http.createServer(function (req, res) {
        // request handling logic...
-       var src = fs.createReadStream(process.argv[3]);
+       var src = fs.createReadStream(path);
        src.pipe(res);
      })
-     server.listen(process.argv[2]);
+     server.listen(port);
 
 /**
  Here's the official solution in case you want to compare notes:
