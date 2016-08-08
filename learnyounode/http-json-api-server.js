5ed1bb44f7a4404384caea0d20a171ab9ffb968b
austincunningham <austincunningham@hotmail.com>
2016-08-08 14:04:50 +0100

    http json api server resolved and offical solution

--- learnyounode/http-json-api-server.js
+++ learnyounode/http-json-api-server.js
@@ -3,7 +3,9 @@ var url = require('url');
 var port = process.argv[2];
      var callBack = function (req, res) {
      var urlObj = url.parse(req.url, true);
-     var date = new Date(urlObj.parse.iso);
+     //console.log(urlObj);
+     var timestmp = urlObj.query.iso;
+     var date = new Date(timestmp);
      console.log("date: "+ date);
 
      if (urlObj.pathname === '/api/parsetime') {
@@ -12,7 +14,11 @@ var port = process.argv[2];
      	res.writeHead(200, { 'Content-Type': 'application/json' });
 
      	console.log(date.getHours());
-     	res.end(JSON.stringify({hour:date.getHours(),minute:date.getMinutes(),second:date.getSeconds()}));
+     	res.end(JSON.stringify({
+     		hour:date.getHours(),
+     		minute:date.getMinutes(),
+     		second:date.getSeconds()
+     	}));
      }
      if (urlObj.pathname === '/api/unixtime') {
      	//console.log(urlObj.pathname);
@@ -23,3 +29,46 @@ var port = process.argv[2];
 }
 var server = http.createServer(callBack);
 server.listen(port);
+
+/**
+ Here's the official solution in case you want to compare notes:
+
+ ─────────────────────────────────────────────────────────────────────────────
+
+     var http = require('http')
+     var url = require('url')
+
+     function parsetime (time) {
+       return {
+         hour: time.getHours(),
+         minute: time.getMinutes(),
+         second: time.getSeconds()
+       }
+     }
+
+     function unixtime (time) {
+       return { unixtime : time.getTime() }
+     }
+
+     var server = http.createServer(function (req, res) {
+       var parsedUrl = url.parse(req.url, true)
+       var time = new Date(parsedUrl.query.iso)
+       var result
+
+       if (/^\/api\/parsetime/.test(req.url))
+         result = parsetime(time)
+       else if (/^\/api\/unixtime/.test(req.url))
+         result = unixtime(time)
+
+       if (result) {
+         res.writeHead(200, { 'Content-Type': 'application/json' })
+         res.end(JSON.stringify(result))
+       } else {
+         res.writeHead(404)
+         res.end()
+       }
+     })
+     server.listen(Number(process.argv[2]))
+
+ ─────────────────────────────────────────────────────────────────────────────
+ */
